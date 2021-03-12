91bd6ea4b6a79730a2960c660cfea125c9b3b557
bacebu4 <vasua14735@icloud.com>
2021-03-12 14:47:17 +0300

    feat: added es build

---
+++ es/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "BaceButton", {
+  enumerable: true,
+  get: function get() {
+    return _BaceButton.BaceButton;
+  }
+});
+
+var _BaceButton = require("./BaceButton");
