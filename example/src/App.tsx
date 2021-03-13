7e7f045f910d8d68325a8263554065dc92860c2f
bacebu4 <vasua14735@icloud.com>
2021-03-13 06:03:41 +0300

    feat: better examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { BaceButton } from "@ui-library/BaceButton";
+import { BaceButton, BaceInput } from "@ui-library/index";
 
 export const App: React.FC = () => {
   return (
-    <>
-      <h1>Hey</h1>
-      <BaceButton>Hey yo</BaceButton>
-      <BaceButton isSolid>Hey yo</BaceButton>
-    </>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "500px",
+        margin: "0 auto",
+        justifyContent: "space-around",
+        height: "100vh",
+        flex: 1,
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        <BaceButton>Usual Button</BaceButton>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <BaceButton isSolid>Solid Button</BaceButton>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        <BaceInput placeholder="Some placeholder" />
+      </div>
+    </div>
   );
 };
